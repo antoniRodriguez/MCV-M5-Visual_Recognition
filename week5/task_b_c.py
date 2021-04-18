da0f6d16b2fffe25ae4c72ece3d8e4cfb7005aea
antoniRodriguez <72408561+antoniRodriguez@users.noreply.github.com>
2021-04-19 01:03:45 +0200

    Change target path in order to run task C or B

--- week5/task_b_c.py
+++ week5/task_b_c.py
@@ -20,7 +20,7 @@ import scipy.misc
 from PIL import Image  
 
 models = ["COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml","COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"]
-path = '/home/group00/working/week5/img_task_C'
+path = '/home/group00/working/week5/img_task_C' # Change path in order to run task C or B
 paths = [os.path.join(path,x) for x in os.listdir(path)]
 
 
@@ -28,7 +28,7 @@ evaluate_differences = False
 save_pkl = False
 if not evaluate_differences:
     for idx,model_path in enumerate(models):
-        output_path = f"/home/group00/working/week5/test_outs_task_C/{model_path[:-5]}/"
+        output_path = f"/home/group00/working/week5/test_outs_task_C/{model_path[:-5]}/" # Change path in order to run task C or B
         os.makedirs(output_path, exist_ok=True)
         
         cfg = pretrained_config(model_url=model_path)
@@ -63,15 +63,15 @@ if not evaluate_differences:
                 dictionary['instances'] = outputs
                 dictionary['images'] = images
 
-                f = open('week5/keyboard_task_C_outs'+str(idx)+'.pkl', 'wb')
+                f = open('week5/keyboard_task_C_outs'+str(idx)+'.pkl', 'wb') # Change path in order to run task C or B
                 pickle.dump(dictionary, f)
                 f.close()
 else:
-    f = open('/home/group00/working/week5/keyboard_task_b_outs.pkl', 'rb')
+    f = open('/home/group00/working/week5/keyboard_task_b_outs.pkl', 'rb') # Change path in order to run task C or B
     keyboard_results_dictionary = pickle.load(f)
     f.close()
 
-    f = open('/home/group00/working/week5/unmodified_task_b_outs.pkl', 'rb')
+    f = open('/home/group00/working/week5/unmodified_task_b_outs.pkl', 'rb') # Change path in order to run task C or B
     original_results_dictionary = pickle.load(f)
     f.close()
 
@@ -93,8 +93,8 @@ else:
             image_name = keyboard_results_dictionary['images'][idx][-16:-4]+'.png'
             maskRcnn = 'COCO-Detection/faster_rcnn_R_50_FPN_3x/'
             fasterRcnn = 'COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/'
-            keyboard = '/home/group00/working/week5/test_outs_task_B_keyboard_dining_table_/'
-            unmodified = '/home/group00/working/week5/test_outs_task_B_unmodified_dining_table_/'
+            keyboard = '/home/group00/working/week5/test_outs_task_B_keyboard_dining_table_/' # Change path in order to run task C or B
+            unmodified = '/home/group00/working/week5/test_outs_task_B_unmodified_dining_table_/' # Change path in order to run task C or B
             keyboard_detection_maskRcnn = cv2.imread(keyboard+maskRcnn+image_name)
             unmodified_detection_maskRcnn = cv2.imread(unmodified+maskRcnn+image_name)
             keyboard_detection_fasterRcnn = cv2.imread(keyboard+fasterRcnn+image_name)
